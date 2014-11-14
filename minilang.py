e08811dd6e04779ec4ca54607a259eb9c4561aa2
Vincent Foley <vfoley@gmail.com>
2014-11-14 15:04:35 -0500

    Use dicts instead of tuples, separate tokens and AST nodes

--- minilang.py
+++ minilang.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import sys
 
+# Token types
 TOK_PRINT  = 0
 TOK_ID     = 1
 TOK_VAR    = 2
@@ -13,22 +16,38 @@ TOK_LPAREN = 9
 TOK_RPAREN = 10
 TOK_COLON  = 11
 
+# AST node categories (we use the word category to not be confused
+# with the word type)
+AST_DECL   = 0
+AST_ASSIGN = 1
+AST_PRINT  = 2
+AST_INT    = 3
+AST_FLOAT  = 4
+AST_ID     = 5
+AST_ADD    = 6
+AST_MUL    = 7
+
 
 def error(msg):
     print("Error: " + msg)
     sys.exit(1)
 
+def tok(ty, val):
+    return { "type": ty, "value": val }
+
+def node(cat, **args):
+    return dict(cat=cat, **args)
 
 def lex(s):
     """
     Input : a string representing a mini program
     Output: a list of tokens
 
-    lex(s) will produce a sequence of tokens, which are tuples of two
-    elements: a type of token (defined above) and a semantic value.
-    The semantic value is a piece of information associated with the
-    token, such as the name of an identifier or the value of an
-    integer literal.  Some tokens, such as the plus symbol, do not
+    lex(s) will produce a sequence of tokens, which are dicts with two
+    bindings: the type of the token (as defined above) and a semantic
+    value.  The semantic value is a piece of information associated
+    with the token, such as the name of an identifier or the value of
+    an integer literal.  Some tokens, like the plus symbol, do not
     have an associated semantic value.
     """
     i = 0
@@ -43,17 +62,17 @@ def lex(s):
 
         # Operators and punctuation
         elif c == "=":
-            tokens.append((TOK_EQ, None))
+            tokens.append(tok(TOK_EQ, None))
         elif c == "+":
-            tokens.append((TOK_PLUS, None))
+            tokens.append(tok(TOK_PLUS, None))
         elif c == "*":
-            tokens.append((TOK_STAR, None))
+            tokens.append(tok(TOK_STAR, None))
         elif c == "(":
-            tokens.append((TOK_LPAREN, None))
+            tokens.append(tok(TOK_LPAREN, None))
         elif c == ")":
-            tokens.append((TOK_RPAREN, None))
+            tokens.append(tok(TOK_RPAREN, None))
         elif c == ":":
-            tokens.append((TOK_COLON, None))
+            tokens.append(tok(TOK_COLON, None))
 
         # Integer literals
         elif c.isdigit():
@@ -68,10 +87,10 @@ def lex(s):
                     num += s[i]
                     i += 1
                 i -= 1
-                tokens.append((TOK_FLOAT, float(num)))
+                tokens.append(tok(TOK_FLOAT, float(num)))
             else:
                 i -= 1
-                tokens.append((TOK_INT, int(num)))
+                tokens.append(tok(TOK_INT, int(num)))
 
         # Identifiers and keywords
         elif c.isalpha() or c == "_":
@@ -81,13 +100,13 @@ def lex(s):
                 i += 1
             i -= 1 # Read one char too many
             if ident == "print":
-                tokens.append((TOK_PRINT, None))
+                tokens.append(tok(TOK_PRINT, None))
             elif ident == "var":
-                tokens.append((TOK_VAR, None))
+                tokens.append(tok(TOK_VAR, None))
             elif ident in ("int", "float"):
-                tokens.append((TOK_TYPE, ident))
+                tokens.append(tok(TOK_TYPE, ident))
             else:
-                tokens.append((TOK_ID, ident))
+                tokens.append(tok(TOK_ID, ident))
         else:
             error("invalid character: %r" % c)
         i += 1
@@ -103,49 +122,62 @@ def parse(toks):
     non-terminal (left-hand side of a ::=) has its own local function
     definition.
 
-        program ::= { decl } { stmt }
-        decl ::= ident ':' type
-        stmt ::= ident '=' expr
-               | 'print' expr
-        expr ::= term '+' expr
-               | term
-        term ::= factor '*' term
-               | factor
-        factor ::= '(' expr ')'
-                 | ident
-                 | int
+        program  ::=  decls stmts
+        decls    ::=  decl decls
+                   |  ε
+        decl     ::=  ident ':' type
+        stmts    ::=  stmt stmts
+                   |  ε
+        stmt     ::=  ident '=' expr
+                   |  'print' expr
+        expr     ::=  term '+' expr
+                   |  term
+        term     ::=  factor '*' term
+                   |  factor
+        factor   ::=  '(' expr ')'
+                   |  ident
+                   |  int
 
-    The AST nodes are represented with tuples as follows:
+    The AST nodes are represented with dicts as follows:
     - Declarations
-        - var x: int     : (TOK_VAR, x, int)
-        - var y: float   : (TOK_VAR, y, float)
+        - var id: type   : { "cat": AST_DECL, id: "x", "type": type }
 
     - Statements
-        - id = expr      : (TOK_EQ, id, expr)
-        - print expr     : (TOK_PRINT, expr)
+        - id = expr      : { "cat": AST_ASSIGN, "lhs": id, "rhs": expr }
+        - print expr     : { "cat": AST_PRINT, "expr": expr }
 
     - Expressions
-        - int            : (TOK_INT, int)    # the actual token
-        - id             : (TOK_ID, id)      # the actual token
-        - e1 + e2        : (TOK_PLUS, e1, e2)
-        - e1 * e2        : (TOK_STAR, e1, e2)
+        - int            : { "cat": AST_INT, "value": int }
+        - float          : { "cat": AST_FLOAT, "value": float }
+        - id             : { "cat": AST_ID, "name": id }
+        - e1 + e2        : { "cat": AST_ADD, "lhs": e1, "rhs": e2 }
+        - e1 * e2        : { "cat": AST_MUL, "lhs": e1, "rhs": e2 }
 
     For example, here is a simple statement and its AST representation:
 
         x = 3 + y
-        (TOK_EQ, "x", (TOK_PLUS, (TOK_INT, 3), (TOK_ID, 4)))
+
+        {
+          "cat": AST_ASSIGN,
+          "lhs": "x",
+          "expr": {
+            "cat": AST_ADD,
+            "lhs": { "cat": AST_INT, "value": 3 },
+            "rhs": { "cat": AST_ID, "id": "y" }
+          }
+        }
     """
 
     def consume(tok_type):
-        if tok_type == toks[0][0]:
+        if tok_type == toks[0]["type"]:
             t = toks.pop(0)
             return t
         else:
-            error("expected %d, found %d" % (tok_type, toks[0][0]))
+            error("expected %d, found %d" % (tok_type, toks[0]["type"]))
 
     def peek():
         if toks:
-            return toks[0][0]
+            return toks[0]["type"]
         else:
             return None
 
@@ -166,10 +198,10 @@ def parse(toks):
     def decl():
         if peek() == TOK_VAR:
             consume(TOK_VAR)
-            _, id = consume(TOK_ID)
+            id = consume(TOK_ID)
             consume(TOK_COLON)
-            _, ty = consume(TOK_TYPE)
-            return (TOK_VAR, id, ty)
+            ty = consume(TOK_TYPE)
+            return node(AST_DECL, id=id["value"], type=ty["value"])
         else:
             error("not a valid declaration")
 
@@ -182,14 +214,14 @@ def parse(toks):
     def stmt():
         next_tok = peek()
         if next_tok == TOK_ID:
-            _, id = consume(TOK_ID)
+            id = consume(TOK_ID)
             consume(TOK_EQ)
             e = expr()
-            return (TOK_EQ, id, e)
+            return node(AST_ASSIGN, lhs=id["value"], rhs=e)
         elif next_tok == TOK_PRINT:
             consume(TOK_PRINT)
             e = expr()
-            return (TOK_PRINT, e)
+            return node(AST_PRINT, expr=e)
         else:
             error("illegal statement")
 
@@ -199,7 +231,7 @@ def parse(toks):
         if next_tok == TOK_PLUS:
             consume(TOK_PLUS)
             e = expr()
-            return (TOK_PLUS, t, e)
+            return node(AST_ADD, lhs=t, rhs=e)
         return t
 
     def term():
@@ -208,7 +240,7 @@ def parse(toks):
         if next_tok == TOK_STAR:
             consume(TOK_STAR)
             t = term()
-            return (TOK_STAR, f, t)
+            return node(AST_MUL, lhs=f, rhs=t)
         return f
 
     def factor():
@@ -220,13 +252,13 @@ def parse(toks):
             return e
         elif next_tok == TOK_INT:
             tok = consume(TOK_INT)
-            return tok
+            return node(AST_INT, value=tok["value"])
         elif next_tok == TOK_FLOAT:
             tok = consume(TOK_FLOAT)
-            return tok
+            return node(AST_FLOAT, value=tok["value"])
         elif next_tok == TOK_ID:
             tok = consume(TOK_ID)
-            return tok
+            return node(AST_ID, name=tok["value"])
         else:
             error("illegal token %d" % next_tok)
 
@@ -243,38 +275,38 @@ def build_symtab(ast):
     report an error.
     """
     symtab = {}
-    for _, id, ty in ast["decls"]:
-        if id in symtab:
-            error("%s is already declared" % id)
+    for decl in ast["decls"]:
+        if decl["id"] in symtab:
+            error("%s is already declared" % decl["id"])
         else:
-            symtab[id] = ty
+            symtab[decl["id"]] = decl["type"]
     return symtab
 
 
 def typecheck(ast, symtab):
     def check_stmt(stmt):
-        if stmt[0] == TOK_PRINT:
-            typed_expr = check_expr(stmt[1])
-            return (TOK_PRINT, typed_expr)
-        elif stmt[0] == TOK_EQ:
-            typed_rhs = check_expr(stmt[2])
-            if typed_rhs[-1] == symtab[stmt[1]]:
-                return (TOK_EQ, stmt[1], typed_rhs)
+        if stmt["cat"] == AST_PRINT:
+            typed_expr = check_expr(stmt["expr"])
+            return node(AST_PRINT, expr=typed_expr)
+        elif stmt["cat"] == AST_ASSIGN:
+            typed_rhs = check_expr(stmt["rhs"])
+            if typed_rhs["type"] == symtab[stmt["lhs"]]:
+                return node(AST_ASSIGN, lhs=stmt["lhs"], rhs=typed_rhs)
             else:
-                error("expected %s, got %s" % (symtab[stmt[1]], typed_rhs[-1]))
+                error("expected %s, got %s" % (symtab[stmt["lhs"]], typed_rhs["type"]))
 
     def check_expr(expr):
-        if expr[0] == TOK_INT:
-            return (TOK_INT, expr[1], "int")
-        elif expr[0] == TOK_FLOAT:
-            return (TOK_FLOAT, expr[1], "float")
-        elif expr[0] == TOK_ID:
-            return (TOK_ID, expr[1], symtab[expr[1]])
-        elif expr[0] in (TOK_PLUS, TOK_STAR):
-            typed_e1 = check_expr(expr[1])
-            typed_e2 = check_expr(expr[2])
-            if typed_e1[-1] == typed_e2[-1]:
-                return (expr[0], typed_e1, typed_e2, typed_e1[-1])
+        if expr["cat"] == AST_INT:
+            return node(AST_INT, value=expr["value"], type="int")
+        elif expr["cat"] == AST_FLOAT:
+            return node(AST_FLOAT, value=expr["value"], type="float")
+        elif expr["cat"] == AST_ID:
+            return node(AST_ID, name=expr["name"], type=symtab[expr["name"]])
+        elif expr["cat"] in (AST_ADD, AST_MUL):
+            typed_e1 = check_expr(expr["lhs"])
+            typed_e2 = check_expr(expr["rhs"])
+            if typed_e1["type"] == typed_e2["type"]:
+                return node(expr["cat"], lhs=typed_e1, rhs=typed_e2, type=typed_e1["type"])
             else:
                 error("operands must have the same type")
 
@@ -298,38 +330,38 @@ def codegen(ast, symtab):
         return "tmp_" + str(curr_tmp)
 
     def gen_stmt(stmt):
-        if stmt[0] == TOK_EQ:
-            if stmt[1] not in symtab:
-                error("undeclared variable: %s" % stmt[1])
+        if stmt["cat"] == AST_ASSIGN:
+            if stmt["lhs"] not in symtab:
+                error("undeclared variable: %s" % stmt["lhs"])
             tmp = new_temp()
-            expr_out = gen_expr(stmt[2], tmp)
-            return expr_out + ["%s = %s;" % (stmt[1], tmp)]
-        elif stmt[0] == TOK_PRINT:
+            expr_out = gen_expr(stmt["rhs"], tmp)
+            return expr_out + ["%s = %s;" % (stmt["lhs"], tmp)]
+        elif stmt["cat"] == AST_PRINT:
             tmp = new_temp()
-            expr_out = gen_expr(stmt[1], tmp)
-            if stmt[1][-1] == "int":
+            expr_out = gen_expr(stmt["expr"], tmp)
+            if stmt["expr"]["type"] == "int":
                 flag = "d"
             else:
                 flag = "f"
             return expr_out + ['printf("%%%s\\n", %s);' % (flag, tmp)]
 
     def gen_expr(expr, result_register):
-        if expr[0] in (TOK_INT, TOK_FLOAT):
-            return ["%s %s = %s;" % (expr[2], result_register, expr[1])]
-        elif expr[0] == TOK_ID:
-            if expr[1] not in symtab:
-                error("undeclared variable: %s" % expr[1])
-            return ["%s %s = %s;" % (expr[2], result_register, expr[1])]
-        elif expr[0] in (TOK_PLUS, TOK_STAR):
+        if expr["cat"] in (AST_INT, AST_FLOAT):
+            return ["%s %s = %s;" % (expr["type"], result_register, expr["value"])]
+        elif expr["cat"] == AST_ID:
+            if expr["name"] not in symtab:
+                error("undeclared variable: %s" % expr["name"])
+            return ["%s %s = %s;" % (expr["type"], result_register, expr["name"])]
+        elif expr["cat"] in (AST_ADD, AST_MUL):
             t1 = new_temp()
-            e1 = gen_expr(expr[1], t1)
+            e1 = gen_expr(expr["lhs"], t1)
             t2 = new_temp()
-            e2 = gen_expr(expr[2], t2)
-            if expr[0] == TOK_PLUS:
+            e2 = gen_expr(expr["rhs"], t2)
+            if expr["cat"] == AST_ADD:
                 op = "+"
             else:
                 op = "*"
-            return e1 + e2 + ["%s %s = %s %s %s;" % (expr[3], result_register, t1, op, t2)]
+            return e1 + e2 + ["%s %s = %s %s %s;" % (expr["type"], result_register, t1, op, t2)]
 
     output = []
 
@@ -353,11 +385,11 @@ def codegen(ast, symtab):
 
 def main():
     src = sys.stdin.read()
-    toks = lex(src)                # source -> tokens
-    ast = parse(toks)              # tokens -> AST
-    symtab = build_symtab(ast)     # AST -> symbol table
-    typed_ast = typecheck(ast, symtab)
-    for s in codegen(typed_ast, symtab): # (AST * symbol table) -> C code
+    toks = lex(src)                      # source -> tokens
+    ast = parse(toks)                    # tokens -> AST
+    symtab = build_symtab(ast)           # AST -> symbol table
+    typed_ast = typecheck(ast, symtab)   # AST -> Typed AST
+    for s in codegen(typed_ast, symtab): # (Typed AST * symbol table) -> C code
         print(s)
 
 if __name__ == "__main__":
